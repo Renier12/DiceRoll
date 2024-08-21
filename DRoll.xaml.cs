1952ab7607695d8236d59a6c5c4f4c85522c6097
Renier <renier.smith@webafrica.org.za>
2024-08-21 12:20:51 +0200

    Basic funtionality added

--- DRoll.xaml.cs
+++ DRoll.xaml.cs
@@ -1,3 +1,6 @@
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Dice_roll;
 
 public partial class DRoll : ContentPage
@@ -7,10 +10,37 @@ public partial class DRoll : ContentPage
 		InitializeComponent();
 	}
 
+    private int number1 = 2;
+    Random randomNumber = new Random();
+
+    public int RandomNumber(int givenNumber)
+    {
+        givenNumber = randomNumber.Next(1, givenNumber);
+        return givenNumber;
+    }
+
+
+
     async void ButtonClicked(object sender, EventArgs e)
     {
-		await LabelOut.RelRotateTo(360, 1000);
-		LabelOut.Text = "Spin?";
+        if (RadioButtonD4.IsChecked == true)
+        {
+            number1 = 4;
+        } else if (RadioButtonD6.IsChecked == true)
+        {
+            number1 = 6;
+        }else if (RadioButtonD8.IsChecked == true)
+        {
+            number1 = 8;
+        }else if (RadioButtonD12.IsChecked == true)
+        {
+            number1 = 12;
+        }else if (RadioButtonD20.IsChecked == true)
+        {
+            number1 = 20;
+        }
+
+        LabelOut.Text = RandomNumber(number1).ToString(); ;
 
     }
 }
---
+++ Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_roll
+{
+    class Functions
+    {
+        /*public int RandomNumber(int givenNumber)
+        {
+            int newNumber = givenNumber++;
+            return newNumber;
+        }*/
+    }
+}
